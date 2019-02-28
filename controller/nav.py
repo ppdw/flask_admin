cd351c66cea60f8b977910fea9d6808f6b22a873
zuomingyu <736291719@qq.com>
2019-02-28 17:18:14 +0800

    `
    `

--- controller/nav.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, redirect, render_template, request, url_for, session, jsonify
-nav_blueprint = Blueprint('nav', __name__, template_folder='templates', static_folder='static')
-
-
-@nav_blueprint.route('/index/')
-def index():
-    nav = request.form['nav_url']
-    print(str(nav))
-    return '1234'
--- controller/partner.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, redirect, render_template, request, url_for, session, jsonify
-partner_blueprint = Blueprint('partner', __name__, template_folder='templates', static_folder='static')
-
-@partner_blueprint.route('/index/')
-def index():
-    return ''
-@partner_blueprint.route('/bill/')
-def bill():
-    return ''
