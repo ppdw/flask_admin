d07e15ce48905704ba5b5c704dec90aace0046ab
pypandas <48081520+pypandas@users.noreply.github.com>
2019-04-25 13:36:38 +0800

    删除管理员

--- controller/admin.py
+++ controller/admin.py
@@ -12,40 +12,42 @@ log_data = {}
 
 @admin_blueprint.route('/index/')
 def index():
-    admin_user = session.get('admin_user')
     admin_info = Admin.query.filter_by().all()
     user_list = []
     for admin_name in admin_info:
         user_list.append(admin_name.UserName)
-    if admin_user != 'vipadmin':
-        user_list.remove('vipadmin')
+    user_list.remove('vipadmin')
     return render_template('Admin_index.html', user_list=user_list)
 
 
 # 管理员列表
 @admin_blueprint.route('/admin_index/', methods=['GET'])
 def admin_index():
-    # page = int(request.args('page'))
+    page = int(request.args['page'])
     param = []
     temp_dict = {}
     if ('admin_info' in request.args) and (request.args['admin_info']):
         admin_info = request.args['admin_info']
         param.append(Admin.UserName == admin_info)
-    # admin_all = Admin.query.filter(*param).order_by(Admin.ID.desc()).paginate(page=1, per_page=15)
-    # admin_all = db.session.query(Admin.UserName, Admin.ID, Agent.AgentName, Role.RoleName, Admin.RegTime,
-    #                    Admin.LastLoginIP, Admin.LastLoginIP).filter(*param).filter(Admin.RoleID == Role.RoleID).all()
-    admin_all = db.session.query(Admin.UserName, Role.RoleName, Admin.ID, Admin.LastLoginIP, Admin.LastLoginTM, Admin.RegTime).filter(Admin.RoleID == Role.RoleID).filter(*param).all()
-    print(admin_all)
-    # pages = admin_all.pages
-    # admin_all = admin_all.items
-    # temp = []
-    # for i in admin_all:
-    #     if i.UserName != 'vipadmin':
-    #         temp.append(i.to_json())
-    # print(temp)
+    admin_all = Admin.query.filter(*param).order_by(Admin.ID.desc()).paginate(page=page, per_page=15)
+    pages = admin_all.pages
+    admin_all = admin_all.items
+    temp = []
+    for i in admin_all:
+        if i.UserName != 'vipadmin':
+            temp.append(i.to_json())
+    for admin_list in temp:
+        if admin_list['RoleID'] != 0:
+            admin_list['RoleID'] = (Role.query.filter_by(RoleID=admin_list['RoleID']).first()).RoleName
+        else:
+            admin_list['RoleID'] = '超级管理员'
+        if admin_list['AgentID'] != 0 | admin_list['AgentID'] != 1:
+            admin_list['AgentID'] = (Agent.query.filter_by(AgentID=admin_list['AgentID']).first()).AgentName
+        else:
+            admin_list['AgentID'] = '平台'
 
-    # temp_dict['data'] = temp
-    # temp_dict['pages'] = pages
+    temp_dict['data'] = temp
+    temp_dict['pages'] = pages
 
     return json.dumps(temp_dict)
 
@@ -74,12 +76,31 @@ def info():
             db.session.commit()
             log_data['ActionName'] = '管理员类控制器'
             log_data['ActionContent'] = '编辑管理员(' + str(admin_info.UserName) + '),管理员ID为' + str(admin_id)
-            # log_data['ConAct'] = 0
             api.adminlogs(log_data)
             if not admin_info:
                 return
             jumpurl = 'www.baidu.com'
             return render_template('Public_success.html', jumpurl=jumpurl)
+
+
+# 删除管理员
+@admin_blueprint.route('/delete_admin/', methods=['GET'])
+def delete_admin():
+    admin_user = session.get('admin_user')
+    delete_id = request.args['delete_id']
+    try:
+        del_id = Admin.query.filter_by(ID=delete_id).first()
+        if del_id.UserName == 'admin':
+            return jsonify({'error': 1, 'msg': '管理员admin不能被删除', 'info': '/admin/admin_index/'})
+        del_name = del_id.UserName
+        db.session.delete(del_id)
+        db.session.commit()
+        log_data['ActionName'] = Permission.ACTION_NAME[5]
+        log_data['ActionContent'] = '管理员' + str(admin_user) + '删除了管理员:' + str(del_name) + '(ID:' + str(delete_id) + ')'
+        api.adminlogs(log_data)
+        return jsonify({'error': 0, 'msg': '删除成功', 'info': '/admin/admin_index/'})
+    except:
+        return jsonify({'error': 1, 'msg': '管理员不存在', 'info': '/admin/admin_index/'})
 
 
 # 登陆验证
@@ -148,8 +169,8 @@ def ajax_log():
     # vipadmin 管理员能看所有日志，包括已删除的日志
     if admin_name != 'vipadmin':
         param.append(Adminactionlog.ConAct == 0)
-    # param.append(Adminactionlog.InputDate >= starttime)
-    # param.append(Adminactionlog.InputDate <= endtime)
+    param.append(Adminactionlog.InputDate >= starttime)
+    param.append(Adminactionlog.InputDate <= endtime)
     log_info = Adminactionlog.query.filter(*param).order_by(Adminactionlog.InputDate.desc()).paginate(page=page,
                                                                                                       per_page=15)
     pages = log_info.pages
@@ -169,7 +190,6 @@ def ajax_log():
 def delete_logs():
     admin_user = session.get('admin_user')
     delete_id = request.args['delete_id']
-    print(delete_id, '@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@')
     try:
         del_id = Adminactionlog.query.filter_by(ID=delete_id).first()
         if del_id.ConAct == 1:
